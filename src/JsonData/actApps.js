3177077e58592061716b27be9d9967320feebe88
Nikhil George <nikhilgrg67@gmail.com>
2022-03-26 10:52:52 +0530

    changed json

--- src/JsonData/actApps.js
+++ src/JsonData/actApps.js
@@ -1,7 +1,7 @@
-const actApps = [{"Interface_Id":"101","Src_App":"PMT","Dest_App":"MDO","Active":"Y"},
-{"Interface_Id":"102","Src_App":"Jedi","Dest_App":"PMT","Active":"Y"},
-{"Interface_Id":"103","Src_App":"MAPS","Dest_App":"PMT","Active":"Y"},
-{"Interface_Id":"104","Src_App":"PISL","Dest_App":"PMT","Active":"Y"},
-{"Interface_Id":"105","Src_App":"RMS","Dest_App":"PMT","Active":"Y"},
-{"Interface_Id":"106","Src_App":"PMT","Dest_App":"MP","Active":"Y"}];
+const actApps = [{"Interface_Id":"101","Src_App":"ABC","Dest_App":"DEF","Active":"Y"},
+{"Interface_Id":"102","Src_App":"GHIJ","Dest_App":"ABC","Active":"Y"},
+{"Interface_Id":"103","Src_App":"KLMN","Dest_App":"ABC","Active":"Y"},
+{"Interface_Id":"104","Src_App":"OPQR","Dest_App":"ABC","Active":"Y"},
+{"Interface_Id":"105","Src_App":"STUV","Dest_App":"ABC","Active":"Y"},
+{"Interface_Id":"106","Src_App":"ABC","Dest_App":"WX","Active":"Y"}];
 export default (actApps);
